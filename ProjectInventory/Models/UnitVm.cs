a6804b7802726ceeb4b5ff20298032c90184a37c
KeYRun DHKL <keyrundhkl@gmail.com>
2026-01-02 07:33:14 +0545

    authenticationimplemented

--- ProjectInventory/Models/UnitVm.cs
+++ ProjectInventory/Models/UnitVm.cs
@@ -11,6 +11,5 @@ public class UnitVm
     [Required]
     [StringLength(5, MinimumLength = 1, ErrorMessage = "Symbol must contain 1 to 5 characters")]
     public string Symbol { get; set; } = string.Empty;
-    
     public string? Description { get; set; }
 }
